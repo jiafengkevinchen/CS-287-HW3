dd8eecc4f7a51c58f6fecb3ea070ab7be3a3ca4b
jiafengkevinchen <jiafeng.k.chen@gmail.com>
2019-03-06 12:53:42 -0500

    change parameter implementation in layernorm

--- models/layernorm.py
+++ models/layernorm.py
@@ -4,13 +4,16 @@ from namedtensor import ntorch, NamedTensor
 from namedtensor.nn import nn as nnn
 
 class LayerNorm(nnn.Module):
-    def __init__(self, features, eps=1e-6):
+    def __init__(self, features, dim, eps=1e-6):
         super().__init__()
-        self.a_2 = nnn.Parameter(torch.ones(features))
-        self.b_2 = nnn.Parameter(torch.zeros(features))
+        self.a_2 = ntorch.ones(features, names=(dim,))
+        self.b_2 = ntorch.ones(features, names=(dim,))
+        self.register_parameter("layernorm.a_2", self.a_2)
+        self.register_parameter("layernorm.b_2", self.b_2)
         self.eps = eps
+        self.dim = dim
 
-    def forward(self, x, dim):
-        mean = x.mean(dim)
-        std = x.std(dim)
+    def forward(self, x):
+        mean = x.mean(self.dim)
+        std = x.std(self.dim)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
