22c3c0d32b19171cfe5f2d607bb90fcabe023cae
jiafengkevinchen <jiafeng.k.chen@gmail.com>
2019-03-06 00:42:40 -0500

    .

--- models/lstm_decoder.py
+++ models/lstm_decoder.py
@@ -79,6 +79,7 @@ class LSTMDecoderAttn(nnn.Module):
         for t in range(embedded.shape["trgSeqlen"]):
 
             a, context = self.attn(ht_flat, H, "embedding", "srcSeqlen")
+            context = ntorch.stack([context], "trgSeqlen")
             word_t = embedded[{'trgSeqlen': slice(t, t+1)}]
             lstm_input = ntorch.cat([word_t, context], "embedding")
             output, (ht, ct) = self.lstm(lstm_input, (ht, ct))
