69371740d40369f3c09d52eef4a68d5301c21246
jiafengkevinchen <jiafeng.k.chen@gmail.com>
2019-03-06 12:44:38 -0500

    .

--- models/sublayer_connection.py
+++ models/sublayer_connection.py
@@ -3,7 +3,7 @@ from torch import nn
 from namedtensor import ntorch, NamedTensor
 from namedtensor.nn import nn as nnn
 from attention import Attention
-from layer_norm import LayerNorm
+from layernorm import LayerNorm
 
 class SublayerConnection(nnn.Module):
     """
