bfb0eabbeba7424d1de3757a8c675abb7cadce99
jiafengkevinchen <jiafeng.k.chen@gmail.com>
2019-03-06 00:03:25 -0500

    self

--- models/attention.py
+++ models/attention.py
@@ -6,7 +6,7 @@ from namedtensor.nn import nn as nnn
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Attention(nnn.Module):
-    def forward(h, H, embedding_dim, src_seqlen_dim):
+    def forward(self, h, H, embedding_dim, src_seqlen_dim):
         log_weights = (h.dot(embedding_dim, H) / (H.size(embedding_dim) ** .5))
         a = log_weights.softmax(src_seqlen_dim)
 
